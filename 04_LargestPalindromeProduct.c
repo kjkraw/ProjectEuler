25b25aae84fc28fcb93a1c74ad8084e2ff4fdfef
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-10-31 11:17:49 -0700

    Finish solution and test. Success!

--- 04_LargestPalindromeProduct.c
+++ 04_LargestPalindromeProduct.c
@@ -1,11 +1,12 @@
 /*Largest Palindrome Product
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 \times 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
+
+RESULT: CORRECT
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /*
 What is the differnce between:
@@ -42,16 +43,12 @@ int isPalindrome(int n) {
 }
 
 int main() {
-    printf("%d\n", reverseInt(12345));
-    return 1;
-
     int factor_a = 0;
     int factor_b = 0;
     int cur_best = 0;
     for (int i = FACTOR_LIMIT; i > 99; i--) {
         for (int j = FACTOR_LIMIT; j > 99; j--) {
             int cur = i * j;
-            printf("%d\n", cur_best);
             if (isPalindrome(cur) && cur > cur_best){
                 cur_best = cur;
                 factor_a = i;
