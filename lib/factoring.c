564ff604d56e87fe509dad6cf2b5454d443b0b02
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-11-04 11:30:42 -0800

    Finished a simple implementation of trial division for factoring numbers

--- lib/factoring.c
+++ lib/factoring.c
@@ -1,43 +1,26 @@
 #include <stdlib.h>
 
+#define ULL unsigned long long
+
 /** Find the factors of a number using trial division.
 This is a very naieve and ineffective way to 
 @param to_factor The number that should be factored
-@param num_factors The number of factors (the length of the array returned)
-@return a pointer to the array of factors (sorted)
+@param factors Pointer to the pointer to the start of the array
+@return num_factors The number of factors (the length of the factors array)
 */
-long long *trialdivision(long long to_factor, int *num_factors) {
-    *num_factors = 1;
-    long long *factors = malloc(*num_factors * sizeof(long long));
-    factors[0] = 1;
+ULL trialdivision(ULL to_factor, ULL **factors) {
+    ULL num_factors = 0;
+    ULL *factors_ptr = NULL;
 
-    for (long long i = 2; i < to_factor; i++){
-        if (to_factor % i == 0){
-            *num_factors++;
-            factors = realloc(factors, *num_factors * sizeof(long long));
-            factors[*num_factors - 1] = i;
+    for (ULL i = 1; i <= to_factor; i++) {
+        if (to_factor % i == 0) {
+            num_factors++;
+            factors_ptr = realloc(factors_ptr, num_factors * sizeof(ULL));
+
+            *(factors_ptr + (num_factors - 1)) = i;
         }
     }
 
-    *num_factors++;
-    factors = realloc(factors, *num_factors * sizeof(long long));
-    factors[*num_factors - 1] = to_factor;
-    return factors;
-}
-
-#include <stdio.h>
-
-int main() {
-    for (int i = 2; i < 100; i++){
-        printf("Factoring %d...\n", i);
-        int num_factors = 0;
-        long long *factors = trialdivision(i, &num_factors);
-        printf("Number of factors: %d | ", num_factors);
-        printf("Factors: [ ");
-        for (int j = 0; j < num_factors; j++) {
-            printf("%lld ", factors[j]);
-        }
-        printf(" ]\n");
-    }
-    return 0;
+    *factors = factors_ptr;
+    return num_factors;
 }
