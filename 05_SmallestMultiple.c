30dcd4ab7d4f4f035cb2cdbcfb151c2cdbd16ee8
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-11-26 12:42:24 -0800

    Again, I find myself with a naive solution that will work, but take forever. I'm going to eat lunch and see if I have any ideas.

--- 05_SmallestMultiple.c
+++ 05_SmallestMultiple.c
@@ -5,3 +5,32 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int TestSolution(long product, long max_factor) {
+    /** Check that all integers from 1 to max_factor are factors of product. */
+    for (long i = 1; i <= max_factor; i++) {
+        if (product % i != 0) return 0;
+    }
+    return 1;
+}
+
+/*
+Naievely, I can increment a number until I hit one for which all others are a factor.
+First hit wins.
+*/
+
+int main() {
+    char in [256];
+    printf("Input max factor: ");
+    gets(in);
+    long max_factor = strtol(in, NULL, 10);
+
+    long current = 0;
+    printf("Current: \n");
+    do{
+        printf("%ld\r", current);
+        current += max_factor;
+    }while(!TestSolution(current, max_factor));
+    printf("\n\nResult: %ld", current);
+    return 1;
+}
