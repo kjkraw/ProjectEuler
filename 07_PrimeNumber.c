4bcf96a77a58bcce6eeb2c62c7d6aaf5d4acea8c
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-12-03 14:52:46 -0800

    A few small changes. It looks like all I really had to do was increase the size of the sieve.

--- 07_PrimeNumber.c
+++ 07_PrimeNumber.c
@@ -1,5 +1,7 @@
 /*
 What is the 10,001st prime number?
+
+RESULT: SUCCESS
 */
 
 #include <stdio.h>
@@ -17,7 +19,7 @@ A prime sieve works by generating a list of integers up to a limit, then removin
 The simplest version to implement is the Sieve of Eratosthenes.
 */
 
-#define ARRAY_SIZE 10000
+#define ARRAY_SIZE 1000000
 #define MARKED 127
 
 typedef struct {
@@ -40,6 +42,7 @@ int append(List *target, long data) {
 }
 
 int generateSieve(long lower_bound, char *sieve, int size) {
+    printf("Generating sieve...\n");
     
     for (int i = 0; i < size; i++){
         if (sieve[i] == MARKED) continue; // Skip marked numbers
@@ -49,14 +52,14 @@ int generateSieve(long lower_bound, char *sieve, int size) {
             break;
         }
 
-        long m = 0;
-        for (long j = 2; m < size + lower_bound; j++){
-            m = p * j;
-            int idx = (int) (m - lower_bound);
+        long m = p + p;
+        while (m < size + lower_bound){
+            int idx = (int) m - lower_bound;
             sieve[idx] = MARKED;
+            m += p;
         }
     }
-
+    printf("Sieve generated!\n");
     return 0;
 }
 
@@ -73,20 +76,17 @@ int generatePrimes(long lower_bound, char *sieve, int sieve_size, List *primes) 
 int main() {
     long lower_bound = 2;
     char sieve [ARRAY_SIZE] = {0};
-    generateSieve(lower_bound, sieve, ARRAY_SIZE);
     List primes = {
         .capacity = 100,
         .size = 0,
         .array = (long*) calloc(100, sizeof(long))
     };
+    
+    generateSieve(lower_bound, sieve, ARRAY_SIZE);
     generatePrimes(lower_bound, sieve, ARRAY_SIZE, &primes);
+    
     printf("Number of primes: %d\n", primes.size);
-
-    printf("[ ");
-    for (int i = 0; i < primes.size; i++) {
-        printf("%ld ", primes.array[i]);
-    }
-    printf("]\n");
+    printf("The 10,001st prime is: %ld", primes.array[10000]);
 
     return 0;
 }
