40419b814b8670b94fcb9a9f428d9864bbeb65be
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-12-08 15:24:59 -0800

    Implement the Sieve of Eratosthenes algorithm using bitwise operations on an array of unsigned integers. This works; unfortunately, to reach the upper bound for the goal, I need an array that is too large for c to allocate it.

--- 10_SumOfPrimes.c
+++ 10_SumOfPrimes.c
@@ -7,7 +7,6 @@ RESULT: INCOMPLETE
 #include <stdlib.h>
 #include <stdio.h>
 
-#define UPPER_BOUND 200000
 
 /*
 I have a worm in my brain saying that I have to try to do this using a bit array.
@@ -40,31 +39,33 @@ Let's start with that; an algorithm that marks all of the multiples of a given i
 
 */
 
-#define MASK_SIZE 32
+#define MASK_SIZE 62500
+#define UPPER_BOUND 2000000
+
 int main(){
+    long long sum = 0;
     unsigned int mask [MASK_SIZE] = {0};
 
-
-    // This section creates a bitmask of all composites of p
-    int p = 2;  
-    unsigned int p_mask [MASK_SIZE] = {0};
-
-    int m = p + p;
-    while (m <= UPPER_BOUND) {
-        int array_idx = (m - 2) / 32;
-        int bit_shift = (m - 2) % 32;
-        p_mask[array_idx] |= 1 << bit_shift;
-    }
-
     // You read a bitmask with a cursor and &
-
-    int natural = 2;
-    for (int natural = 2; natural <= UPPER_BOUND; natural++) {
-        int narray_idx = (natural - 2) / 32;
-        int nbit_shift = (natural - 2) % 32;
-        unsigned int cursor = 1 << nbit_shift;
-        if (mask[narray_idx] & cursor == 0) { // This means the number at this spot is NOT MARKED
-
+    for (int p = 2; p <= UPPER_BOUND; p++) {
+        int outer_idx = (p - 2) / 32;
+        int outer_shift = (p - 2) % 32;
+        unsigned int cursor = 1 << outer_shift;
+        if (!(mask[outer_idx] & cursor)) {
+            sum += p;
+            // This section creates a bitmask of all composites of p.
+            unsigned int p_mask [MASK_SIZE] = {0};
+            int m = p + p;
+            while (m <= UPPER_BOUND) {
+                int array_idx = (m - 2) / 32;
+                int bit_shift = (m - 2) % 32;
+                p_mask[array_idx] |= 1 << bit_shift;
+                m += p;
+            }
+            for (int i = 0; i < MASK_SIZE; i++){
+                // The bitmask of p composites is added to the main mask.
+                mask[i] |= p_mask[i];
+            }
         }
     }
 
