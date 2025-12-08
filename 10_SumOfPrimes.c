098299568429a3096c85f79d299418d35f0a961c
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-12-08 15:55:14 -0800

    Using calloc to allocate heap memory for the arrays works. I guess the other method allocates on the stack, which is too small.

--- 10_SumOfPrimes.c
+++ 10_SumOfPrimes.c
@@ -1,7 +1,7 @@
 /*
 Find the sum of all primes under 2 million.
 
-RESULT: INCOMPLETE
+RESULT: SUCCESS
 */
 
 #include <stdlib.h>
@@ -9,34 +9,11 @@ RESULT: INCOMPLETE
 
 
 /*
-I have a worm in my brain saying that I have to try to do this using a bit array.
+I've already made a prime sieve before, so I wanted to try something a little different.
+Here, I used a bitmask as the sieve for the Sieve of Eratosthenes algorithm.
+This means that the algorithm is mostly made up of bitwise operations.
 
-Here's the idea:
-Each 0 bit is prime, each 1 bit is composite.
-The first bit is associated with 2.
-The last with UPPER_BOUND.
-
-*/
-
-/*
-I have a handful of operations
-bitwise and &
-bitwise or  |
-bitwise xor ^
-negation    ~
-
-l shift     <<
-r shift     >>
-
-The algorithm looks like this:
-
-1. let p be the first prime number in the series
-2. move p spaces
-3. mark
-4. repeat 2 & 3 until at the end of the series
-
-Let's start with that; an algorithm that marks all of the multiples of a given integer.
-
+This was a fun challenge!
 */
 
 #define MASK_SIZE 62500
@@ -44,7 +21,7 @@ Let's start with that; an algorithm that marks all of the multiples of a given i
 
 int main(){
     long long sum = 0;
-    unsigned int mask [MASK_SIZE] = {0};
+    unsigned int * mask = calloc(MASK_SIZE, sizeof(unsigned int));
 
     // You read a bitmask with a cursor and &
     for (int p = 2; p <= UPPER_BOUND; p++) {
@@ -54,7 +31,7 @@ int main(){
         if (!(mask[outer_idx] & cursor)) {
             sum += p;
             // This section creates a bitmask of all composites of p.
-            unsigned int p_mask [MASK_SIZE] = {0};
+            unsigned int * p_mask = calloc(MASK_SIZE, sizeof(unsigned int));
             int m = p + p;
             while (m <= UPPER_BOUND) {
                 int array_idx = (m - 2) / 32;
@@ -66,7 +43,9 @@ int main(){
                 // The bitmask of p composites is added to the main mask.
                 mask[i] |= p_mask[i];
             }
+            free(p_mask);
         }
     }
 
+    printf("Sum: %lld \n", sum);
 }
